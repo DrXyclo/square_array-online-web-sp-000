54731a011155c857122cdbc46206096f37ad9332
Dr. XyClo <sdcarmody@gmail.com>
2018-12-30 23:49:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
-def square_array(array)
-  # your code here
+numbers = [1, 2, 3]
+
+def square_array(numbers)
+  numbers.each do |i|
+     puts i*i 
+    
+    
+  end 
+  #puts numbers.inspect
+ # return new
+ # puts new
 end
+
+square_array(numbers)
