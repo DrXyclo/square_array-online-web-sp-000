cafe81be2568d3b4a6a3896ed2e8ddf54af3beec
Dr. XyClo <sdcarmody@gmail.com>
2018-12-30 23:53:31 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,9 +3,9 @@ numbers = [1, 2, 3]
 def square_array(numbers)
   squared_numbers = []
   numbers.each do |i| 
-    numbers << i*i 
+    squared_numbers << i*i 
     end 
-  numbers 
+  squared_numbers 
 end
 
 square_array(numbers)
